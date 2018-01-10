a25262eb8e98abefd0f6105ff1e4416bb187a0aa
chengxin6 <chengxin6@xdf.cn>
2018-01-10 10:58:52 +0800

    完善样式

--- html/js/echartsData.js
+++ html/js/echartsData.js
@@ -1,166 +1,159 @@
 // 项目整体 饼状图
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main'));
-
-// 指定图表的配置项和数据
-option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        x: 'right',
-        y: 'center',
-        orient: 'vertical',
-        data: ['男', '女']
-    },
-    series: [
-        {
-            name: '性别占比',
-            type: 'pie',
-            hoverOffset: 1,
-            radius: [50, 125],
-            center: ['50%', '45%'],
-            roseType: 'radius',
-            clockwise: false,
-            label: {
-                normal: {
-                    show: false
+function pieFm() {
+    // 指定图表的配置项和数据
+    option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            x: 'right',
+            y: 'center',
+            orient: 'vertical',
+            data: ['男', '女']
+        },
+        series: [
+            {
+                name: '性别占比',
+                type: 'pie',
+                hoverOffset: 1,
+                radius: [50, 125],
+                center: ['50%', '45%'],
+                roseType: 'radius',
+                clockwise: false,
+                label: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: false
+                    }
                 },
-                emphasis: {
-                    show: false
-                }
-            },
-            lableLine: {
-                normal: {
-                    show: false
+                lableLine: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: true
+                    }
                 },
-                emphasis: {
-                    show: true
-                }
-            },
-            data: [
-                { value: 10, name: '男' },
-                { value: 9, name: '女' }
-            ]
-        }
-    ],
-    color: ['#ffbf00', '#26c1d8']
-};
+                data: [
+                    { value: 10, name: '男' },
+                    { value: 9, name: '女' }
+                ]
+            }
+        ],
+        color: ['#ffbf00', '#26c1d8']
+    };
 
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
-
-
-
-
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main1'));
-
-// 指定图表的配置项和数据
-option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        x: 'right',
-        y: 'center',
-        orient: 'vertical',
-        data: ['组1', '组2']
-    },
-    series: [
-        {
-            name: '年龄分布',
-            type: 'pie',
-            hoverOffset: 1,
-            radius: [50, 125],
-            center: ['50%', '45%'],
-            roseType: 'radius',
-            clockwise: false,
-            label: {
-                normal: {
-                    show: false
+    // 指定图表的配置项和数据
+    option1 = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            x: 'right',
+            y: 'center',
+            orient: 'vertical',
+            data: ['组1', '组2']
+        },
+        series: [
+            {
+                name: '年龄分布',
+                type: 'pie',
+                hoverOffset: 1,
+                radius: [50, 125],
+                center: ['50%', '45%'],
+                roseType: 'radius',
+                clockwise: false,
+                label: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: false
+                    }
                 },
-                emphasis: {
-                    show: false
-                }
-            },
-            lableLine: {
-                normal: {
-                    show: false
+                lableLine: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: true
+                    }
                 },
-                emphasis: {
-                    show: true
-                }
-            },
-            data: [
-                { value: 10, name: '组1' },
-                { value: 9, name: '组2' }
-            ]
-        }
-    ],
-    color: ['#ffbf00', '#26c1d8']
-};
+                data: [
+                    { value: 10, name: '组1' },
+                    { value: 9, name: '组2' }
+                ]
+            }
+        ],
+        color: ['#ffbf00', '#26c1d8']
+    };
 
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
-
-
-
-// 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('main2'));
-
-// 指定图表的配置项和数据
-option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        x: 'right',
-        y: 'center',
-        orient: 'vertical',
-        data: ['组1', '组2', '组3']
-    },
-    series: [
-        {
-            name: '组别',
-            type: 'pie',
-            hoverOffset: 1,
-            radius: [50, 125],
-            center: ['50%', '45%'],
-            roseType: 'radius',
-            clockwise: false,
-            label: {
-                normal: {
-                    show: false
+    // 指定图表的配置项和数据
+    option2 = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            x: 'right',
+            y: 'center',
+            orient: 'vertical',
+            data: ['组1', '组2', '组3']
+        },
+        series: [
+            {
+                name: '组别',
+                type: 'pie',
+                hoverOffset: 1,
+                radius: [50, 125],
+                center: ['50%', '45%'],
+                roseType: 'radius',
+                clockwise: false,
+                label: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: false
+                    }
                 },
-                emphasis: {
-                    show: false
-                }
-            },
-            lableLine: {
-                normal: {
-                    show: false
+                lableLine: {
+                    normal: {
+                        show: false
+                    },
+                    emphasis: {
+                        show: true
+                    }
                 },
-                emphasis: {
-                    show: true
-                }
-            },
-            data: [
-                { value: 10, name: '组1' },
-                { value: 9, name: '组2' },
-                { value: 5, name: '组3' }
-            ]
-        }
-    ],
-    color: ['#ffbf00', '#26c1d8', '#999999']
-};
+                data: [
+                    { value: 10, name: '组1' },
+                    { value: 9, name: '组2' },
+                    { value: 5, name: '组3' }
+                ]
+            }
+        ],
+        color: ['#ffbf00', '#26c1d8', '#999999']
+    };
 
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+    var myChart1 = echarts.init(document.getElementById('main1'));
+    var myChart2 = echarts.init(document.getElementById('main2'));
+    var myChart3 = echarts.init(document.getElementById('main3'));
+    var myChart4 = echarts.init(document.getElementById('main4'));
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    myChart1.setOption(option1);
+    myChart2.setOption(option2);
+    myChart3.setOption(option2);
+    myChart4.setOption(option2);
+}
 
+//对比柱状图
 
 
 
@@ -305,27 +298,27 @@ myChart1.setOption(option1, true);
 
 
 
-function hotLook(){
-    
+function hotLook() {
+
     var myChart = echarts.init(document.getElementById('hotLook'));
-    
+
     var data = [
-        [13,50,1000000000,'China',1990],
-        [5,70,2000000000,'China',2015]
+        [13, 50, 1000000000, 'China', 1990],
+        [5, 70, 2000000000, 'China', 2015]
     ];
-    
+
     option = {
         /*legend: {
             right: 10,
             data: ['1990', '2015']
         },*/
-    grid: {
-        left: '3%',
-        right: '5%',
-        top: '15%',
-        bottom: '5%',
-        containLabel: true
-    },
+        grid: {
+            left: '3%',
+            right: '5%',
+            top: '15%',
+            bottom: '5%',
+            containLabel: true
+        },
         xAxis: {
             splitLine: {
                 lineStyle: {
@@ -370,61 +363,61 @@ function hotLook(){
             }
         }]
     };
-    
+
     myChart.setOption(option);
 
 }
 
 
-function singleFm(){
-    		// 基于准备好的dom，初始化echarts实例
-		var myChart = echarts.init(document.getElementById('r2b0'));
-		var myChart1 = echarts.init(document.getElementById('r2b1'));
-		option = {
-			color: ['#3398DB'],
-			tooltip: {
-				trigger: 'axis',
-				axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-					type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-				}
-			},
-			grid: {
-				left: '3%',
-				right: '4%',
-				bottom: '3%',
-				top:'15%',
-				containLabel: true
-			},
-			xAxis: [
-				{
-					type: 'category',
-					data: ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13'],
-					axisTick: {
-						alignWithLabel: true
-					}
-				}
-			],
-			yAxis: [
-				{
-					type: 'value',
-					name:'用时(s)'
-				}
-			],
-			series: [
-				{
-					name: '任务用时',
-					type: 'bar',
-					barWidth: '60%',
-					data: [10, 52, 200, 334, 390, 330, 220, 52, 200, 334, 390, 330, 220]
-				}
-			]
-		};
-		// 使用刚指定的配置项和数据显示图表。
-		myChart.setOption(option);
-		var option1=option;
-		option1.yAxis[0].name='数量(个)'
-		option1.series[0].name='错误数'
-		myChart1.setOption(option1);
+function singleFm() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('r2b0'));
+    var myChart1 = echarts.init(document.getElementById('r2b1'));
+    option = {
+        color: ['#3398DB'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            top: '15%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12', 'p13'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                name: '用时(s)'
+            }
+        ],
+        series: [
+            {
+                name: '任务用时',
+                type: 'bar',
+                barWidth: '60%',
+                data: [10, 52, 200, 334, 390, 330, 220, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    };
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    var option1 = option;
+    option1.yAxis[0].name = '数量(个)'
+    option1.series[0].name = '错误数'
+    myChart1.setOption(option1);
 }
 
 
